e30ad023d289ca7ed889b29ba665ec71fc5c6831
tasac-sainz <tssainzdeaja@gmail.com>
2025-07-09 12:40:54 +0200

    jhasta el cuarto ejercicio hecho

--- modulo-2-ejercicios-js/src/js/main.js
+++ modulo-2-ejercicios-js/src/js/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+//inicio ejercicios de variables
+
+//EJERCICIO 1:
+document.querySelector("h1").innerHTML = "Esta página no es compatible con la versión actual de tu navegador. Por favor actualízalo a la versión más reciente";
+
+//EJERCICIO 2:
+document.querySelector(".hello").innerHTML = "Hola, mundo";
+
+//EJERCICIO 3:
+document.querySelector("h2").innerHTML="la Adalaber seleccionada es:";
+document.querySelector("ul").innerHTML="Kaski";
+
+//EJERCICIO 4:
+document.querySelector(".title").innerHTML="Los Podencos"
+document.querySelector(".image").src = "https://images.unsplash.com/photo-1549930098-2bc4cd0aac53?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+document.querySelector(".subtitle").innerHTML="los PODENCOS son perros maravillosos";
